5ef3954a2759154682b27c1df8414b179aa9f00c
Leomar Vaz Sartor <leomar.sartor@funcionalcorp.com.br>
2026-01-05 15:09:35 -0300

    global css

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+// import QualquerCoisa from './App'
+import { App } from './App'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    {/* <QualquerCoisa /> */}
     <App />
+    {/* <h1> Olá Mundo</h1> */}
   </StrictMode>,
 )
