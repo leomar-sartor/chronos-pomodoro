566f97b506704b1b8fcf76f7b83fc8c506da9a9d
Leomar Vaz Sartor <leomar.sartor@funcionalcorp.com.br>
2026-01-08 14:35:52 -0300

    definição de seções mais estilização container e content

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,3 @@
-import { Heading } from "./components/Heading";
-import { TimerIcon } from "lucide-react";
-
 import "./styles/theme.css";
 import "./styles/global.css";
 
@@ -8,15 +5,38 @@ export function App() {
   console.log("App component rendered");
   return (
     <>
-      <Heading> Olá Mundo 
-        <button>
-          <TimerIcon />
-        </button>
-      </Heading>
+      <div className="container">
+        <div className="content">
+          <section>
+            LOGO
+          </section>
+        </div>
+      </div>
 
-      <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusamus dolorum temporibus, iste labore ipsum voluptate. Qui, necessitatibus sequi ex aspernatur quibusdam quia adipisci dolor veritatis porro atque, distinctio accusamus quo?
-      </p>
+      <div className="container">
+        <div className="content">
+          <section>
+            MENU
+          </section>
+        </div>
+      </div>
+
+
+      <div className="container">
+        <div className="content">
+          <section>
+            FORM
+          </section>
+        </div>
+      </div>
+
+      <div className="container">
+        <div className="content">
+          <section>
+            FOOTER
+          </section>
+        </div>
+      </div>
     </>
   );
 }
