1645522e52db02307ca74fa3d4f0c873724c6a5a
Leomar Vaz Sartor <leomar.sartor@funcionalcorp.com.br>
2026-01-21 14:27:47 -0300

    useRef e ref: forma nao controlada e nao reenderiza o componente

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -2,16 +2,16 @@ import { PlayCircleIcon } from "lucide-react";
 import { Cycles } from "../Cycles";
 import { DefaultButton } from "../DefaultButton";
 import { DefaultInput } from "../DefaultInput";
-import { useState } from "react";
+import { useRef } from "react";
 
 export function MainForm() {
 
-  const [taskName, setTaskname] = useState('');
+  const taskNameInput = useRef<HTMLInputElement>(null);
 
   function handleCreateNewTask(event: React.FormEvent<HTMLFormElement>){
     event.preventDefault();
 
-    console.log('Deu Boas', taskName);
+    console.log('Deu Boas', taskNameInput.current.value);
   }
   
   return (
@@ -19,8 +19,7 @@ export function MainForm() {
 
           <div className="formRow">
             <DefaultInput labelText="Task" id="meuInput" type="text" placeholder="Digite algo" 
-            value={taskName}
-            onChange={e => setTaskname(e.target.value)}
+            ref={taskNameInput}
             />
           </div>
 
