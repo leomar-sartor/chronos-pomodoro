c22336a4f8a4b45873f19ac76fecdb8adef13e31
Leomar Vaz Sartor <leomar.sartor@funcionalcorp.com.br>
2026-01-22 11:01:57 -0300

    iterando elementos com map

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -68,9 +68,11 @@ export function MainForm() {
         <p> Próximo intervalo é de 25 min</p>
       </div>
 
-      <div className="formRow">
-        <Cycles />
-      </div>
+      {state.currentCycle > 0 && (
+        <div className="formRow">
+          <Cycles />
+        </div>
+      )}
 
       <div className="formRow">
         <DefaultButton icon={<PlayCircleIcon />} />
