202ea941798235b73f0c69dc031927e46e7c0b34
Leomar Vaz Sartor <leomar.sartor@funcionalcorp.com.br>
2026-01-23 16:21:58 -0300

    exemplo de enum com task actions e task reducer

--- src/contexts/TaskContext/TaskContextProvider.tsx
+++ src/contexts/TaskContext/TaskContextProvider.tsx
@@ -1,4 +1,4 @@
-import { useReducer, useState } from "react";
+import { useState } from "react";
 import { initialTaskState } from "./initialTaskState";
 import { TaskContext } from "./TaskContext";
 
@@ -10,44 +10,9 @@ export function TaskContextProvider({ children }: TaskContextProviderProps) {
 
   const [state, setState] = useState(initialTaskState);
 
-  type ActionType ={
-    type: string;
-    payload?: number;
-  }
-
-  const [myState, dispatch] = useReducer(
-    (state, action : ActionType) => {
-
-    console.log(state, action);
-
-    switch (action.type) {
-      case 'INCREMENT':{
-
-        if(!action.payload) return state;
-
-        return {
-          ...state, secondsRemaining: state.secondsRemaining + action.payload
-        };
-      }
-    }
-
-    return state;
-  }, {
-    secondsRemaining : 0,
-  }); //valor inicial de numero
-
   return (
     <TaskContext.Provider value={{ state, setState }}>
-      {/* {children} */}
-      <h1>
-        O estado é: {JSON.stringify(myState)}
-      </h1>
-
-      <button onClick={() => dispatch({
-        type: 'INCREMENT',
-        payload: 10
-      })}> Incrementar </button>
-      
+      {children}
     </TaskContext.Provider>
   );
 }
