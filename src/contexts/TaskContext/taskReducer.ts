569fc036aa52cdd6f420d434b0b0e35b52015572
Leomar Vaz Sartor <leomar.sartor@funcionalcorp.com.br>
2026-01-27 14:08:51 -0300

    Limpando o histórico do estado e voltando ao estado inicial

--- src/contexts/TaskContext/taskReducer.ts
+++ src/contexts/TaskContext/taskReducer.ts
@@ -2,6 +2,7 @@ import type { TaskStateModel } from "../../models/TaskStateModel";
 import { formatSecondsToMinutes } from "../../utils/formatSecondsToMinutes";
 import { getNextCycle } from "../../utils/getNextCycle";
 import type { TaskActionModel } from './taskActions';
+import { initialTaskState } from './initialTaskState';
 import { TaskActionTypes } from './taskActions';
 
 export function taskReducer(
@@ -52,7 +53,7 @@ export function taskReducer(
       };
     }
     case TaskActionTypes.RESET_STATE: {
-      return state;
+      return { ...initialTaskState };
     }
     case TaskActionTypes.COUNT_DOWN: {
       return {
